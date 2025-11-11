5a9da91eab3960f2e1c7a105dc58cede66d3e9e8
hadessharma <scsa990@gmail.com>
2025-11-11 11:31:28 -0700

    Completed data model

---
+++ models/story.py
@@ -0,0 +1,37 @@
+'''
+Stroy name
+theme
+first option
+children: [go_left, go_right]
+'''
+
+from sqlalchemy import Column, Integer, String, DateTime, JSON, Boolean, ForeignKey
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+from db.database import Base
+
+class Story(Base):
+    __tablename__ = "stories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    session_id = Column(String, index=True)
+    create_at = Column(DateTime(timezone=True), server_default=func.now())
+    
+    nodes = relationship("StotyNode", back_populates="story")
+
+class StoryNode(Base):
+    __tablename__ = "story_nodes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    story_id = Column(Integer, ForeignKey("stories.id"), index=True)
+    content = Column(String)
+    is_root = Column(Boolean, default=False)
+    is_ending = Column(Boolean, default=False)
+    is_winning_ending = Column(Boolean, default=False)
+    options = Column(JSON, default=list)
+
+    story = relationship("Story", back_populates="nodes")
+
+    
